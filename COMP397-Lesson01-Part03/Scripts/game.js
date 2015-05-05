cbed1d555a99f3bbe5587b24f8803cbf1bafd4d1
guashtiSingh <ashtisingh@hotmail.com>
2015-05-05 11:25:10 -0400

    added create js structure

--- COMP397-Lesson01-Part03/Scripts/game.js
+++ COMP397-Lesson01-Part03/Scripts/game.js
@@ -1,9 +1,32 @@
-﻿/*
+﻿/// <reference path="typings/easeljs/easeljs.d.ts" />
+/*
 // Difference of using the functions var init = function (). It is creating an anonymous function
 var init = function () {
 }
 */
+var stage;
+var canvas = document.getElementById("canvas");
+
 function init() {
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.on("tick", gameLoop);
+
+    main();
+}
+
+function gameLoop() {
+    stage.update();
+}
+
+function main() {
     console.log("Game is running...");
+
+    var helloText = new createjs.Text("Hello World!", "40px Consolas", "#FFFF00");
+
+    helloText.x = 30;
+    helloText.y = 200;
+
+    stage.addChild(helloText);
 }
 //# sourceMappingURL=game.js.map
--- COMP397-Lesson01-Part03/Scripts/game.ts
+++ COMP397-Lesson01-Part03/Scripts/game.ts
@@ -1,12 +1,35 @@
-﻿
+﻿/// <reference path="typings/easeljs/easeljs.d.ts" />
+
 /*
 // Difference of using the functions var init = function (). It is creating an anonymous function
 var init = function () {
 } 
 */
 
+var stage: createjs.Stage; 
+var canvas = document.getElementById("canvas"); //creates a reference
+
 function init() {
-    console.log("Game is running...");
+    stage = new createjs.Stage(canvas);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.on("tick", gameLoop);
+
+    main();
 }
 
+function gameLoop() {
+
+    stage.update();
+}
+
+function main() {
+    console.log("Game is running...");
+
+    var helloText: createjs.Text = new createjs.Text("Hello World!", "40px Consolas", "#FFFF00"); 
+
+    helloText.x = 30;
+    helloText.y = 200;
+
+    stage.addChild(helloText);
+}
 
